c38a4d557d078cc2eb4a8562cac26cbe05009a16
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2022-01-27 10:53:13 -0300

    get collection products in realtime

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,19 +1,42 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList} from 'react-native';
+import {firebase} from '@react-native-firebase/firestore';
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
+import {styles} from './styles';
+import {Product, ProductProps} from '../Product';
 
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import {shoppingListExample} from '../../utils/shopping.list.data';
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>();
+
+  const _loadProducts = async () => {
+    const subscribe = await firebase
+      .firestore()
+      .collection('products')
+      .onSnapshot(querySnapshot => {
+        const product = querySnapshot.docs.map(doc => {
+          return {
+            id: doc.id,
+            ...doc.data(),
+          };
+        }) as ProductProps[];
+
+        setProducts(product);
+      });
+
+    return () => subscribe();
+  };
+
+  useEffect(() => {
+    _loadProducts();
+  }, []);
 
   return (
     <FlatList
-      data={shoppingListExample}
+      data={products}
       keyExtractor={item => item.id}
-      renderItem={({ item }) => <Product data={item} />}
+      renderItem={({item}) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
       contentContainerStyle={styles.content}
